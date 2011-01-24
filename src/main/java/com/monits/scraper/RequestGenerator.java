e5001e6945f11ff4cd0fc22eb66f1d637de6d043
Nicolas de Bernardi <ndebernardi@monits.com>
2011-01-24 14:08:46 +0000

    This is the class that JMR needs  to do the method of the Request Generator

---
+++ src/main/java/com/monits/scraper/RequestGenerator.java
@@ -0,0 +1,63 @@
+package com.monits.scraper;
+
+public class RequestGenerator {
+	
+	
+	
+	private String verb;
+	
+	private String url;
+	private String userAgent;
+	private String session;
+	private String cookie;
+	
+	public RequestGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public String getVerb() {
+		return verb;
+	}
+
+	public void setVerb(String verb) {
+		this.verb = verb;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUserAgent() {
+		return userAgent;
+	}
+
+	public void setUserAgent(String userAgent) {
+		this.userAgent = userAgent;
+	}
+
+	public String getSession() {
+		return session;
+	}
+
+	public void setSession(String session) {
+		this.session = session;
+	}
+
+	public String getCookie() {
+		return cookie;
+	}
+
+	public void setCookie(String cookie) {
+		this.cookie = cookie;
+	}
+
+	
+	
+	
+
+}
