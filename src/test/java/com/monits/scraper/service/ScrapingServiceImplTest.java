9faeeb4d4639fca929e64d2652901663181c8826
José Manuel Rolón <jmrolon@monits.com>
2011-01-27 19:48:32 +0000

    Now trans is also a mock (it was hard >.<). Added test to check the type of exception thrown by the scraping method.

--- src/test/java/com/monits/scraper/service/ScrapingServiceImplTest.java
+++ src/test/java/com/monits/scraper/service/ScrapingServiceImplTest.java
@@ -16,15 +16,15 @@ import java.util.Map;
 
 import junit.framework.Assert;
 
+import org.easymock.Capture;
 import org.easymock.EasyMock;
+import org.easymock.IAnswer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import com.monits.scraper.requests.RequestGenerator;
 import com.monits.scraper.requests.RequestVerb;
-import com.monits.scraper.transformation.Identity;
 import com.monits.scraper.transformation.Transformation;
 
 /**
@@ -40,8 +40,6 @@ import com.monits.scraper.transformation.Transformation;
 */
 public class ScrapingServiceImplTest {
 
-	
-	private Transformation trans = new Identity();
 	private ScrapingServiceImpl sService;
 	
 	@Before
@@ -55,21 +53,25 @@ public class ScrapingServiceImplTest {
 	
 	/**
 	 * Test for the method ScrapingService.scrap(RequestGenerator rGen, 
-	 * Transformation transform)
-	 * Check the return with two different types of UserAgent from a page 
-	 * who support both types
-	 * The returns obviously have to be different
+	 * Transformation transform),
+	 * Check the returns with two different types of UserAgent from a page 
+	 * who support both types ,the returns obviously have to be different
 	 * 
 	 * @throws ScrapingServiceException
 	 */
 	@Test
 	public void testScrapMobilePcBrowser() throws ScrapingServiceException {
 
-		
 		RequestGenerator requestFF = EasyMock
 			.createMock(RequestGenerator.class);
 		RequestGenerator requestMobile = EasyMock
 			.createMock(RequestGenerator.class);
+		Transformation trans = EasyMock.createMock(Transformation.class);
+		
+		String responseFFUserAgent;
+		String responseMobileUserAgent;
+	
+		final Capture<String> xhtmlCapture = new Capture<String>();
 		
 		EasyMock.expect(requestFF.getUrl())
 			.andReturn("http://www.minijuegos.com").anyTimes();
@@ -77,27 +79,41 @@ public class ScrapingServiceImplTest {
 			.andReturn("Mozilla Firefox 3.2.1.2.3").anyTimes();
 		EasyMock.expect(requestFF.getCookies())
 			.andReturn(null).anyTimes();
-		EasyMock.expect(requestFF.getVerb()).andReturn(RequestVerb.GET).anyTimes();
+		EasyMock.expect(requestFF.getVerb()).andReturn(RequestVerb.GET)
+			.anyTimes();
 		
 		EasyMock.expect(requestMobile.getUrl())
 			.andReturn("http://www.minijuegos.com").anyTimes();
 		EasyMock.expect(requestMobile.getUserAgent())
-			.andReturn("Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us").times(4);
+			.andReturn("Mozilla/5.0 (iPhone; U; CPU iPhone " +
+			"OS 4_0 like Mac OS X; en-us").anyTimes();
 		EasyMock.expect(requestMobile.getCookies())
 			.andReturn(null).anyTimes();
 		EasyMock.expect(requestMobile.getVerb())
 			.andReturn(RequestVerb.GET).anyTimes();
 		
-		EasyMock.replay(requestFF, requestMobile);
+		EasyMock.expect(trans.transform(EasyMock.capture(xhtmlCapture)))
+		.andAnswer(new IAnswer<String>() {
+
+			@Override
+			public String answer() throws Throwable {
+				return xhtmlCapture.getValue();
+			}
+			
+		}).anyTimes();
 		
-		Assert.assertNotNull("Firefox Request Failed", sService.scrap(requestFF, trans));
-		String pruebaPcBrowser = sService.scrap(requestFF, trans);
+		EasyMock.replay(requestFF, requestMobile, trans);
+		
+		Assert.assertNotNull("Firefox Request Failed", sService
+				.scrap(requestFF, trans));
+		
+		responseFFUserAgent = sService.scrap(requestFF, trans);
 		
 		Assert.assertNotNull("Mobile Request Failed", sService
 					.scrap(requestMobile, trans));
-		String pruebaMobileBrowser = sService.scrap(requestMobile, trans);
+		responseMobileUserAgent = sService.scrap(requestMobile, trans);
 		
-		Assert.assertNotSame(pruebaMobileBrowser, pruebaPcBrowser);
+		Assert.assertNotSame("Both response equals, Failed" ,responseFFUserAgent, responseMobileUserAgent);
 	}
 	
 	/**
@@ -116,10 +132,12 @@ public class ScrapingServiceImplTest {
 			.createMock(RequestGenerator.class);
 		RequestGenerator requestWithoutCookie = EasyMock
 			.createMock(RequestGenerator.class);
+		Transformation trans = EasyMock.createMock(Transformation.class);
 		
 		String pruebaConCookie;
 		String pruebaSinCookie;
 		
+	
 		Map<String, String> cookie = new HashMap<String, String>();
 		cookie.put("guid", "1F074B19C51E42C5A5175C6F7D");
 		
@@ -133,8 +151,6 @@ public class ScrapingServiceImplTest {
 		EasyMock.expect(requestCookie.getVerb())
 			.andReturn(RequestVerb.GET).anyTimes();
 		
-		EasyMock.replay(requestCookie);
-		
 		EasyMock.expect(requestWithoutCookie.getUrl())
 			.andReturn("http://www.cotodigital.com.ar/novedades.asp")
 				.anyTimes();
@@ -145,7 +161,19 @@ public class ScrapingServiceImplTest {
 		EasyMock.expect(requestWithoutCookie.getVerb())
 			.andReturn(RequestVerb.GET).anyTimes();
 		
-		EasyMock.replay(requestWithoutCookie);
+		final Capture<String> xhtmlCapture = new Capture<String>();
+		
+		EasyMock.expect(trans.transform(EasyMock.capture(xhtmlCapture)))
+			.andAnswer(new IAnswer<String>() {
+
+				@Override
+				public String answer() throws Throwable {
+					return xhtmlCapture.getValue();
+				}
+				
+			}).anyTimes();
+		
+		EasyMock.replay(requestWithoutCookie, requestCookie, trans);
 		
 		pruebaConCookie = sService.scrap(requestCookie, trans);
 		Assert.assertNotNull("Request With Cookie Failed",sService
@@ -155,8 +183,34 @@ public class ScrapingServiceImplTest {
 		Assert.assertNotNull("Request Without Cookie Failed",sService
 				.scrap(requestWithoutCookie, trans));
 		
-		Assert.assertNotSame(pruebaConCookie, pruebaSinCookie);
+		Assert.assertNotSame("Both response equals, Failed", pruebaConCookie, pruebaSinCookie);
 	
+	}
+	
+	/**
+	 * Test for the method ScrapingService.scrap(RequestGenerator rGen,
+	 * Transformation transform)
+	 * Check the exception thrown, it has to be one of our custom 
+	 * class {@link ScrapingServiceException}
+	 * 
+	 * @throws ScrapingServiceException
+	 */
+	@Test(expected = ScrapingServiceException.class)
+	public void testScrapingServiceWronlUrl() throws ScrapingServiceException {
+		
+		RequestGenerator request = EasyMock.createMock(RequestGenerator.class);
+		Transformation trans = EasyMock.createMock(Transformation.class);
+		
+		EasyMock.expect(request.getUrl())
+			.andReturn("http://www.minijuegdd2323dos.com").anyTimes();
+		EasyMock.expect(request.getUserAgent())
+			.andReturn("Mozilla Firefox 3.2.1.2.3").anyTimes();
+		EasyMock.expect(request.getCookies())
+			.andReturn(null).anyTimes();
+		EasyMock.expect(request.getVerb()).andReturn(RequestVerb.GET).anyTimes();
+		
+		sService.scrap(request, trans);
+		
 	}
 	
 }
