b45b0189cc4330c3ec9df8bf5e43861378361849
Ricardo Gomez <rgomez@monits.com>
2011-02-10 20:36:53 +0000

    Arreglo ver noticias de un equipo.

--- src/main/java/com/monits/scraper/transformation/XSLTTransformation.java
+++ src/main/java/com/monits/scraper/transformation/XSLTTransformation.java
@@ -39,112 +39,119 @@ import org.w3c.dom.Document;
  * 
  * @author Ricardo Gomez <rgomez@monits.com>
  * @copyright 2011 Monits
- * @license   Copyright (C) 2011. All rights reserved
- * @version   Release: 1.0.0
- * @link      http://www.monits.com/
- * @since     1.0.0
+ * @license Copyright (C) 2011. All rights reserved
+ * @version Release: 1.0.0
+ * @link http://www.monits.com/
+ * @since 1.0.0
  */
 public class XSLTTransformation implements Transformation {
-	
+
 	private static final int DEFAULT_INITIAL_BUFFER_SIZE = 10000;
-	
+
 	protected Transformer xslt;
-	
+
 	/**
 	 * Parameterized constructor, it builds the URI of the XSL that will be used
 	 * to transform the retrieved XHTML.
 	 * 
-	 * @param xslt The path to the XSL file.
+	 * @param xslt
+	 *            The path to the XSL file.
 	 */
 	public XSLTTransformation(String xslt) {
-		
+
 		URI xsltUri;
 		try {
 			xsltUri = new URI(xslt);
 		} catch (URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		InputStream stream;
 		try {
 			stream = new FileInputStream(new File(xsltUri));
 		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		Source xsltSource = new StreamSource(stream);
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		TransformerFactory transformerFactory = TransformerFactory
+				.newInstance();
 		try {
 			this.xslt = transformerFactory.newTransformer(xsltSource);
 		} catch (TransformerConfigurationException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 	}
-	
+
 	@Override
 	public String transform(String xhtml) {
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(DEFAULT_INITIAL_BUFFER_SIZE);
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(
+				DEFAULT_INITIAL_BUFFER_SIZE);
 		Result result = new StreamResult(outputStream);
-		
+
 		Source source = getSource(xhtml);
-		
+
 		try {
 			xslt.transform(source, result);
-		} catch (TransformerException e) { 
+		} catch (TransformerException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		String output;
 		try {
 			output = outputStream.toString("UTF8");
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		return output;
 	}
-	
+
 	/**
 	 * Generate a source from a string containing the XHTML.
 	 * 
-	 * @param xhtml The XHTML to be contained in the source.
+	 * @param xhtml
+	 *            The XHTML to be contained in the source.
 	 * @return The source created.
 	 * @throws RuntimeException
 	 */
 	private Source getSource(String xhtml) {
-		DocumentBuilderFactory docBuildFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilderFactory docBuildFactory = DocumentBuilderFactory
+				.newInstance();
 		docBuildFactory.setValidating(false);
 		docBuildFactory.setExpandEntityReferences(false);
 		try {
-			docBuildFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			docBuildFactory
+					.setFeature(
+							"http://apache.org/xml/features/nonvalidating/load-external-dtd",
+							false);
 		} catch (ParserConfigurationException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		DocumentBuilder docBuilder;
 		try {
 			docBuilder = docBuildFactory.newDocumentBuilder();
 		} catch (ParserConfigurationException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		InputStream stream;
 		try {
 			stream = new ByteArrayInputStream(xhtml.getBytes("UTF-8"));
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		Document document;
 		try {
 			document = docBuilder.parse(stream);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		return new DOMSource(document);
 	}
-	
 }
